9573a8c0ec95c0b2b08fa7c294b51c6b424f7dad
Anders Sveen <as@ztlpay.io>
2022-09-10 20:21:37 +0200

    Working version with parsing

--- src/test/kotlin/ParsingTest.kt
+++ src/test/kotlin/ParsingTest.kt
@@ -1,94 +1,64 @@
 import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.annotation.JsonTypeInfo
-import com.fasterxml.jackson.annotation.JsonValue
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.DeserializationConfig
-import com.fasterxml.jackson.databind.JavaType
-import com.fasterxml.jackson.databind.SerializationConfig
-import com.fasterxml.jackson.databind.annotation.JsonTypeResolver
-import com.fasterxml.jackson.databind.jsontype.*
-import com.fasterxml.jackson.databind.jsontype.impl.AsWrapperTypeDeserializer
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.assertAll
 import kotlin.test.Test
 
-//@JsonTypeResolver(EmailResolver::class)
 sealed class Email {
-    data class ValidEmail(val user: String, val domain: String)
-    data class InvalidEmail(val value: String) {
-        companion object {
-            @JvmStatic
-            @JsonCreator
-            fun create(createValue: String): InvalidEmail {
-                return InvalidEmail(createValue)
+    data class ValidEmail(val user: String, val domain: String) : Email()
+    data class InvalidEmail(val value: String, val messages: List<String>) : Email()
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun create(createValue: String): Email {
+            return if (createValue.contains("@")) {
+                createValue.split("@").let { ValidEmail(it.first(), it.last()) }
+            } else {
+                InvalidEmail(createValue, listOf("Not a valid Email ;)"))
             }
         }
     }
 
-}
-
-class EmailResolver: TypeResolverBuilder<EmailResolver> {
-    override fun getDefaultImpl(): Class<*> {
-        return Email.InvalidEmail::class.java
-    }
-
-    override fun buildTypeSerializer(
-        config: SerializationConfig?,
-        baseType: JavaType?,
-        subtypes: MutableCollection<NamedType>?
-    ): TypeSerializer {
-        TODO("Not yet implemented")
-    }
-
-    override fun buildTypeDeserializer(
-        config: DeserializationConfig?,
-        baseType: JavaType?,
-        subtypes: MutableCollection<NamedType>?
-    ): TypeDeserializer {
-        TODO("Not yet implemented")
-    }
-
-    override fun init(idType: JsonTypeInfo.Id?, res: TypeIdResolver?): EmailResolver {
-        TODO("Not yet implemented")
-    }
-
-    override fun inclusion(includeAs: JsonTypeInfo.As?): EmailResolver {
-        TODO("Not yet implemented")
-    }
-
-    override fun typeProperty(propName: String?): EmailResolver {
-        TODO("Not yet implemented")
-    }
-
-    override fun defaultImpl(defaultImpl: Class<*>?): EmailResolver {
-        TODO("Not yet implemented")
-    }
-
-    override fun typeIdVisibility(isVisible: Boolean): EmailResolver {
-        TODO("Not yet implemented")
-    }
 
 }
 
-data class RegistrationForm(val email: Email.InvalidEmail, val anonymous: Boolean, val name: String)
+data class RegistrationForm(val email: Email, val anonymous: Boolean, val name: String)
 
 class ParsingTest {
 
     @Test
-    fun testShouldParseBasicJson() {
+    fun testShouldParseInvalidEmail() {
         val mapper = jacksonObjectMapper()
-        val testJson = """
-            {
-                "email": "hello",
-                "anonymous": false,
-                "name": "Myname"
-            }
-        """.trimIndent()
-        val parsed: RegistrationForm = mapper.readValue(testJson)
+        val parsed: RegistrationForm = mapper.readValue(getTestJson("hello"))
 
-        assertThat(parsed.email.value).isEqualTo("hello")
+        when (parsed.email) {
+            is Email.InvalidEmail -> assertThat(parsed.email.value).isEqualTo("hello")
+            else -> throw RuntimeException("Hello")
+        }
     }
 
+    @Test
+    fun testShouldParseValidEmail() {
+        val mapper = jacksonObjectMapper()
+        val parsed: RegistrationForm = mapper.readValue(getTestJson("hello@hello.com"))
+
+        when (parsed.email) {
+            is Email.ValidEmail -> assertAll(
+                { assertThat(parsed.email.user).isEqualTo("hello") },
+                { assertThat(parsed.email.domain).isEqualTo("hello.com") }
+            )
+
+            else -> throw RuntimeException("Hello")
+        }
+    }
+
+    private fun getTestJson(email: String) = """
+                {
+                    "email": "$email",
+                    "anonymous": false,
+                    "name": "Myname"
+                }
+            """.trimIndent()
 }
