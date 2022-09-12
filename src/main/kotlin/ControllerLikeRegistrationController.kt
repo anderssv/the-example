868d89c9df39e4f3102aa42482faf6410371cb9d
Anders Sveen <anders@f12.no>
2022-09-12 19:40:34 +0200

    Introduced address to see how combinations play out

--- src/main/kotlin/ControllerLikeRegistrationController.kt
+++ src/main/kotlin/ControllerLikeRegistrationController.kt
@@ -2,9 +2,18 @@ import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
+interface InvalidDataClass {
+    fun hasErrors(): Boolean = getErrors().isNotEmpty()
+    fun getErrors(): List<String>
+}
+
 sealed class Email {
     data class ValidEmail(val user: String, val domain: String) : Email()
-    data class InvalidEmail(val value: String, val messages: List<String>) : Email()
+    data class InvalidEmail(val value: String, val messages: List<String>) : Email(), InvalidDataClass {
+        override fun getErrors(): List<String> {
+            return messages
+        }
+    }
 
     companion object {
         @JvmStatic
@@ -19,28 +28,59 @@ sealed class Email {
     }
 }
 
+sealed class Address {
+    data class ValidAddress(val streetName: String, val city: String, val poCode: String, val country: String) :
+        Address()
 
-sealed class RegistrationForm {
-    data class InvalidAnonymousRegistrationForm(
-        val email: Email, val anonymous: Boolean, val name: String?, val errors: List<String>
-    ) : RegistrationForm()
-
-    data class ValidAnonymousRegistrationForm(val email: Email.ValidEmail) : RegistrationForm()
-    data class ValidRegistrationForm(val email: Email.ValidEmail, val name: String) : RegistrationForm()
+    data class InvalidAddress(
+        val streetName: String?,
+        val city: String?,
+        val poCode: String?,
+        val country: String?,
+        val messages: List<String>
+    ) :
+        Address(), InvalidDataClass {
+        override fun getErrors(): List<String> {
+            return this.messages
+        }
+    }
 
     companion object {
         @JvmStatic
         @JsonCreator
-        fun create(email: Email, anonymous: Boolean, name: String?): RegistrationForm {
-            return when (email) {
-                is Email.InvalidEmail -> InvalidAnonymousRegistrationForm(email, anonymous, name, email.messages)
-                is Email.ValidEmail -> if (anonymous && name == null) {
-                    ValidAnonymousRegistrationForm(email)
-                } else if (name != null && !anonymous) {
-                    ValidRegistrationForm(email, name)
-                } else {
-                    InvalidAnonymousRegistrationForm(email, anonymous, name, listOf("Invalid combination"))
-                }
+        fun create(streetName: String?, city: String?, poCode: String?, country: String?): Address {
+            return if (streetName.isNullOrEmpty() || city.isNullOrEmpty() || poCode.isNullOrBlank() || country.isNullOrBlank()) {
+                InvalidAddress(streetName, city, poCode, country, listOf("Missing values"))
+            } else {
+                ValidAddress(streetName, city, poCode, country)
+            }
+        }
+    }
+
+}
+
+sealed class RegistrationForm {
+    data class InvalidAnonymousRegistrationForm(
+        val email: Email, val anonymous: Boolean, val name: String?, val address: Address?, val errors: List<String>
+    ) : RegistrationForm()
+
+    data class ValidAnonymousRegistrationForm(val email: Email.ValidEmail) : RegistrationForm()
+    data class ValidRegistrationForm(val email: Email.ValidEmail, val name: String, val address: Address.ValidAddress) :
+        RegistrationForm()
+
+    companion object {
+        @JvmStatic
+        @JsonCreator
+        fun create(email: Email, anonymous: Boolean, name: String?, address: Address?): RegistrationForm {
+            val errors = listOf(email, address).filterIsInstance<InvalidDataClass>().map { it.getErrors() }.flatten()
+            return if (errors.isNotEmpty()) {
+                InvalidAnonymousRegistrationForm(email, anonymous, name, address, errors)
+            } else if (anonymous) {
+                ValidAnonymousRegistrationForm(email as Email.ValidEmail)
+            } else if (name != null) {
+                ValidRegistrationForm(email as Email.ValidEmail, name, address as Address.ValidAddress)
+            } else {
+                InvalidAnonymousRegistrationForm(email, anonymous, name, address, listOf("Invalid combination!"))
             }
         }
     }
--- src/test/kotlin/ParsingTest.kt
+++ src/test/kotlin/ParsingTest.kt
@@ -1,4 +1,3 @@
-import Email.ValidEmail
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.assertj.core.api.Assertions.assertThat
@@ -22,16 +21,69 @@ class ParsingTest {
     }
 
     @Test
+    fun testShouldParseMissingAddress() {
+        val mapper = jacksonObjectMapper()
+        val parsed: RegistrationForm = mapper.readValue(getTestJson("hello", addressJson = null))
+
+        assertThat(parsed).isExactlyInstanceOf(RegistrationForm.InvalidAnonymousRegistrationForm::class.java)
+        (parsed as RegistrationForm.InvalidAnonymousRegistrationForm).let {
+            assertThat(it.email).isExactlyInstanceOf(Email.InvalidEmail::class.java)
+            (it.email as Email.InvalidEmail).let {
+                assertThat(it.value).isEqualTo("hello")
+            }
+            assertThat(it.address).isNull()
+        }
+    }
+
+    @Test
+    fun testShouldParseValidAddress() {
+        val mapper = jacksonObjectMapper()
+        val parsed: RegistrationForm = mapper.readValue(getTestJson("hello"))
+
+        assertThat(parsed).isExactlyInstanceOf(RegistrationForm.InvalidAnonymousRegistrationForm::class.java)
+        (parsed as RegistrationForm.InvalidAnonymousRegistrationForm).let {
+            assertThat(it.email).isExactlyInstanceOf(Email.InvalidEmail::class.java)
+            (it.email as Email.InvalidEmail).let {
+                assertThat(it.value).isEqualTo("hello")
+            }
+            assertThat(it.address).isNotNull
+            assertThat(it.address).isExactlyInstanceOf(Address.ValidAddress::class.java)
+            assertThat((it.address as Address.ValidAddress).city).isEqualTo("Oslo")
+        }
+    }
+
+    @Test
+    fun testShouldParseInvalidAddress() {
+        val mapper = jacksonObjectMapper()
+        val parsed: RegistrationForm = mapper.readValue(
+            getTestJson(
+                "hello", """
+                  "address": { "city": "Oslo" }
+                """.trimIndent()
+            )
+        )
+
+        assertThat(parsed).isExactlyInstanceOf(RegistrationForm.InvalidAnonymousRegistrationForm::class.java)
+        (parsed as RegistrationForm.InvalidAnonymousRegistrationForm).let {
+            assertThat(it.email).isExactlyInstanceOf(Email.InvalidEmail::class.java)
+            (it.email as Email.InvalidEmail).let {
+                assertThat(it.value).isEqualTo("hello")
+            }
+            assertThat(it.address).isNotNull
+            assertThat(it.address).isExactlyInstanceOf(Address.InvalidAddress::class.java)
+            assertThat((it.address as Address.InvalidAddress).city).isEqualTo("Oslo")
+        }
+    }
+
+    @Test
     fun testShouldParseValidEmail() {
         val mapper = jacksonObjectMapper()
         val parsed: RegistrationForm = mapper.readValue(getTestJson("hello@hello.com"))
 
         assertThat(parsed).isExactlyInstanceOf(RegistrationForm.ValidRegistrationForm::class.java)
         (parsed as RegistrationForm.ValidRegistrationForm).let {
-            assertAll(
-                { assertThat(it.email.user).isEqualTo("hello") },
-                { assertThat(it.email.domain).isEqualTo("hello.com") }
-            )
+            assertAll({ assertThat(it.email.user).isEqualTo("hello") },
+                { assertThat(it.email.domain).isEqualTo("hello.com") })
         }
     }
 
@@ -46,11 +98,23 @@ class ParsingTest {
         }
     }
 
-    private fun getTestJson(email: String) = """
+    private fun getTestJson(email: String, addressJson: String? = getAddressJson()) = """
                 {
                     "email": "$email",
+                    ${if (addressJson != null) "$addressJson," else ""}
                     "anonymous": false,
-                    "name": "Myname"
+                    "name": "Myname"   
                 }
             """.trimIndent()
+
+    private fun getAddressJson(): String {
+        return """
+            "address": {
+                "streetName": "test",
+                "city": "Oslo",
+                "poCode": "0164",
+                "country": "Norway"
+            }
+        """.trimIndent()
+    }
 }
