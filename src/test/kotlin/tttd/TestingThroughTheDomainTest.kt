e8e62925df47578ca34f6adf33408d93bbed0047
Anders Sveen <anders@f12.no>
2023-11-29 08:14:09 +0100

    Just some docs

--- src/test/kotlin/tttd/TestingThroughTheDomainTest.kt
+++ src/test/kotlin/tttd/TestingThroughTheDomainTest.kt
@@ -15,26 +15,47 @@ class TestingThroughTheDomainTest {
     private val customerRepo = CustomerRepositoryFake()
     private val appService = ApplicationService(applicationRepo, notificationRepo, customerRepo)
 
+    /**
+     * This test sets up the _data_ needed to run the test, and then verifies the result.
+     *
+     * This kind of test is more brittle when the system changes, and is currently failing
+     * because storage of customers was introduced.
+     */
     @Test
     fun testDataOrientedTest() {
         val application = Application.valid()
 
+        // Data is set up, store directly in DB. Ignoring anything else the
+        // system does to reach the state.
         applicationRepo.addApplication(application)
         appService.approveApplication(application.id)
 
         assertThat(applicationRepo.getApplication(application.id).status).isEqualTo(ApplicationStatus.APPROVED)
     }
 
+    /**
+     * This test sets up the _system_ in the correct state.
+     *
+     * It is less brittle when the system changes, and did not break when the storage
+     * of customers was introduced.
+     */
     @Test
     fun testDomainOrientedTest() {
         val application = Application.valid()
 
+        // Start the process of registering application, thus manipulating through the system and
+        // getting everything in a consistent state
         appService.registerInitialApplication(application)
         appService.approveApplication(application.id)
 
         assertThat(applicationRepo.getApplication(application.id).status).isEqualTo(ApplicationStatus.APPROVED)
     }
 
+    /**
+     * This is actually a bad test, but was used to introduce
+     * customer storage, as well as making sure the two previous
+     * ones did not have to change to show how they would break.
+     */
     @Test
     fun testAddActiveCustomerWhenNewApplication() {
         val application = Application.valid()
