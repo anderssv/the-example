78ec16c5e01d3cbc4fec10e1c0e320f08c06648f
Anders Sveen <anders@f12.no>
2025-04-05 09:27:33 +0200

    Inlined test data setup

--- src/test/kotlin/brreg/BrregClientFake.kt
+++ src/test/kotlin/brreg/BrregClientFake.kt
@@ -26,69 +26,53 @@ class BrregClientFake : BrregClient {
 fun BrregEntity.Companion.valid(
     organisasjonsnummer: String = "123456789",
     navn: String = "Test Entity",
-    organisasjonsform: OrganisasjonsformDto = OrganisasjonsformDto(
-        kode = "AS",
-        beskrivelse = "Aksjeselskap",
-        links = null
-    ),
-    registreringsdatoEnhetsregisteret: String = "2021-01-01",
-    registrertIMvaregisteret: Boolean = true,
-    naeringskode1: NaeringskodeDto? = NaeringskodeDto(
-        kode = "62.010",
-        beskrivelse = "Programmeringstjenester",
-        links = null
-    ),
-    antallAnsatte: Int? = 10,
-    forretningsadresse: AdresseDto? = AdresseDto(
-        adresse = listOf("Testveien 1"),
-        postnummer = "0123",
-        poststed = "OSLO",
-        kommunenummer = "0301",
-        kommune = "OSLO",
-        landkode = "NO",
-        land = "Norge"
-    ),
-    stiftelsesdato: String? = "2020-01-01",
-    institusjonellSektorkode: InstitusjonellSektorkodeDto? = InstitusjonellSektorkodeDto(
-        kode = "2100",
-        beskrivelse = "Private aksjeselskaper mv.",
-        links = null
-    ),
-    registrertIForetaksregisteret: Boolean = true,
-    registrertIStiftelsesregisteret: Boolean = false,
-    registrertIFrivillighetsregisteret: Boolean = false,
-    konkurs: Boolean = false,
-    underAvvikling: Boolean = false,
-    underTvangsavviklingEllerTvangsopplosning: Boolean = false,
-    maalform: String = "NB",
-    harRegistrertAntallAnsatte: Boolean = false,
-    links: List<LinkDto>? = listOf(
-        LinkDto(
-            href = "https://data.brreg.no/enhetsregisteret/api/enheter/123456789",
-            rel = "self",
-            type = "application/json"
-        )
-    )
+    antallAnsatte: Int? = 10
 ): BrregEntity {
     return BrregEntity(
         organisasjonsnummer = organisasjonsnummer,
         navn = navn,
-        organisasjonsform = organisasjonsform,
-        registreringsdatoEnhetsregisteret = registreringsdatoEnhetsregisteret,
-        registrertIMvaregisteret = registrertIMvaregisteret,
-        naeringskode1 = naeringskode1,
+        organisasjonsform = OrganisasjonsformDto(
+            kode = "AS",
+            beskrivelse = "Aksjeselskap",
+            links = null
+        ),
+        registreringsdatoEnhetsregisteret = "2021-01-01",
+        registrertIMvaregisteret = true,
+        naeringskode1 = NaeringskodeDto(
+            kode = "62.010",
+            beskrivelse = "Programmeringstjenester",
+            links = null
+        ),
         antallAnsatte = antallAnsatte,
-        forretningsadresse = forretningsadresse,
-        stiftelsesdato = stiftelsesdato,
-        institusjonellSektorkode = institusjonellSektorkode,
-        registrertIForetaksregisteret = registrertIForetaksregisteret,
-        registrertIStiftelsesregisteret = registrertIStiftelsesregisteret,
-        registrertIFrivillighetsregisteret = registrertIFrivillighetsregisteret,
-        konkurs = konkurs,
-        underAvvikling = underAvvikling,
-        underTvangsavviklingEllerTvangsopplosning = underTvangsavviklingEllerTvangsopplosning,
-        maalform = maalform,
-        harRegistrertAntallAnsatte = harRegistrertAntallAnsatte,
-        links = links
+        forretningsadresse = AdresseDto(
+            adresse = listOf("Testveien 1"),
+            postnummer = "0123",
+            poststed = "OSLO",
+            kommunenummer = "0301",
+            kommune = "OSLO",
+            landkode = "NO",
+            land = "Norge"
+        ),
+        stiftelsesdato = "2020-01-01",
+        institusjonellSektorkode = InstitusjonellSektorkodeDto(
+            kode = "2100",
+            beskrivelse = "Private aksjeselskaper mv.",
+            links = null
+        ),
+        registrertIForetaksregisteret = true,
+        registrertIStiftelsesregisteret = false,
+        registrertIFrivillighetsregisteret = false,
+        konkurs = false,
+        underAvvikling = false,
+        underTvangsavviklingEllerTvangsopplosning = false,
+        maalform = "NB",
+        harRegistrertAntallAnsatte = false,
+        links = listOf(
+            LinkDto(
+                href = "https://data.brreg.no/enhetsregisteret/api/enheter/123456789",
+                rel = "self",
+                type = "application/json"
+            )
+        )
     )
 }
